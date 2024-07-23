e61f13b6dfc621d0e3ed3694c331cb83c71205cc
Marvellous <ifezueyoung@gmail.com>
2024-07-23 16:37:33 -0700

    add x account link

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -61,7 +61,7 @@
                         <span class="sr-only">GitHub</span>
                     </div>
                 </a>
-                <a target="_blank" rel="noreferrer" href="">
+                <a target="_blank" rel="noreferrer" href="http://x.com/yungifez">
                     <div
                         class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 py-2 w-9 px-0">
                         <i class="fa-brands fa-x-twitter"></i>
