e7ee3fcd895dc8c10843822c3a23491e981f0caf
Marvellous <ifezueyoung@gmail.com>
2025-06-02 17:12:03 +0100

    Fix broken copy button

--- resources/views/components/copy-button.blade.php
+++ resources/views/components/copy-button.blade.php
@@ -6,8 +6,9 @@
         }
     }" x-effect="hasCopied && setTimeout(() => {
     hasCopied = false
-    }, 2000)" size="icon" variant="ghost" {{$attributes->class(['justify-center h-6 w-6 text-zinc-50 hover:bg-zinc-700
-    hover:text-zinc-50'])}}
+    }, 2000)" size="icon" variant="ghost" {{$attributes->class(['justify-center h-6 w-6 dark:text-zinc-50
+    dark:hover:bg-zinc-700
+    dark:hover:text-zinc-50'])}}
     @click="copyToClipBoard($el.getAttribute('value'))" value="{{$attributes->Get('value')}}">
     <span class="sr-only">Copy</span>
     <i class="far fa-copy" x-show="!hasCopied"></i>
